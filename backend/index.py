3e93d446904e7ab94d1eaa6618ff508760b5b88c
Dilek <95539879+dilekyalcin@users.noreply.github.com>
2024-01-24 18:32:15 +0300

    fix: resolve all conflicts

--- backend/index.py
+++ backend/index.py
@@ -1,26 +1,28 @@
 from flask import Flask, jsonify
 from models.users import Users
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from routes import user_bp, post_bp, comment_bp, like_bp, auth_bp
-from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt
 import json
 import os
 
-app = Flask(__name__)
-jwt = JWTManager(app)
-CORS(app, resources={r'/*': { 'origins': 'http://localhost:5173'}})
 
-# token_blacklist = set()
+app = Flask(__name__)
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 config_file_path = os.path.join(current_directory, 'config.json')
-
 
 with open(config_file_path, 'r') as f:
     config_data = json.load(f)
     for key, value in config_data.items():  
         app.config[key] = value
+
+cors_resources = app.config.get('CORS_RESOURCES', {})
+CORS(app, resources=cors_resources)
+
+
+jwt = JWTManager(app)
+token_blacklist = set()
 
 @jwt.user_identity_loader
 def user_identity_lookup(user):
@@ -32,21 +34,26 @@ def user_lookup_callback(_jwt_header, jwt_data):
     return Users.objects.get(id=identity)
 
 
+@jwt.token_in_blocklist_loader
+def check_if_token_in_blacklist(jwt_header, jwt_data):
+    jti = jwt_data["jti"]
+    return jti in token_blacklist
 
-# @jwt.token_in_blocklist_loader
-# def check_if_token_in_blacklist(decrypted_token, *args, **kwargs):
-#     try:
-#         jti = decrypted_token["jti"]
-#         return jti in token_blacklist
-#     except KeyError:
-#         # jti anahtarı bulunamadı
-#         return True
+
+@app.route('/logout', methods=['DELETE'])
+@cross_origin()
+@jwt_required()
+def logout():
+    jti = get_jwt()['jti']
+    token_blacklist.add(jti)
+    return jsonify({"msg": "Successfully logged out"}), 200
 
 app.register_blueprint(user_bp, url_prefix='/user')
 app.register_blueprint(post_bp, url_prefix='/post')
 app.register_blueprint(comment_bp, url_prefix='/comment')
 app.register_blueprint(like_bp, url_prefix='/like')
 app.register_blueprint(auth_bp, url_prefix='/auth')
+
 
 if __name__ == "__main__":
     app.run(debug = True)
