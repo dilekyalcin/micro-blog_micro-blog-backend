66a004f1d76a73cde0708e51c1d53177cce6779e
Dilek <95539879+dilekyalcin@users.noreply.github.com>
2024-01-24 18:32:15 +0300

    resolved all conflicts (#11)

--- backend/routes/userRoute.py
+++ backend/routes/userRoute.py
@@ -3,6 +3,7 @@ from models.dbContext import connect_to_mongodb
 from flask_jwt_extended import jwt_required, get_jwt_identity, current_user
 from models.users import Users 
 from models.post import Post
+from flask_cors import cross_origin
 import datetime
 import hashlib
 import secrets
@@ -13,7 +14,17 @@ user_bp = Blueprint('user', __name__)
 
 @user_bp.route("/update_user", methods=['PUT'])
 @jwt_required()
+@cross_origin()
 def update_user():
+    """
+    Updates information about the authenticated user.
+
+    Retrieves user information, validates the current user's existence,
+    and updates the user's information.
+
+    Returns:
+        jsonify: A JSON response indicating the success or failure of the user update.
+    """
     data = request.get_json()
     
     user_id = get_jwt_identity()
@@ -33,7 +44,6 @@ def update_user():
         set__firstname=data.get('firstname', current_user.firstname),
         set__lastname=data.get('lastname', current_user.lastname),
         set__username=data.get('username', current_user.username),
-        set__password=data.get('password', current_user.password),
         set__email=data.get('email', current_user.email),
         set__bio=data.get('bio', current_user.bio),
  
@@ -42,14 +52,49 @@ def update_user():
     return {"message": "User updated."}, 200
 
 def create_password_hash(password):
+    """
+    Generates a password hash and salt.
+
+    Args:
+        password (str): The password to hash.
+
+    Returns:
+        tuple: A tuple containing the password salt and hash.
+    """
     password_salt = secrets.token_hex(16)
     password_hash = hashlib.sha256((password + password_salt).encode('utf-8')).hexdigest()
     
     return password_salt, password_hash
 
+@user_bp.route("/get_user_info", methods=['GET'])
+@jwt_required()
+@cross_origin()
+def get_user_info():
+    """
+    Retrieves information about the authenticated user.
+
+    Returns:
+        jsonify: A JSON response containing information about the authenticated user.
+    """
+    user = Users.objects(username=current_user.username).first()
+
+    if user:
+        user_info ={
+            "firstname": user.firstname,
+            "lastname": user.lastname,
+         }
+        return jsonify(user_info), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
 
 @user_bp.route("/get_user_profile", methods=['GET'])
 def get_user_profile():
+    """
+    Retrieves information about a specific user's profile.
+
+    Returns:
+        jsonify: A JSON response containing information about the specified user's profile.
+    """
     username = request.args.get('username', '')
 
     user = Users.objects(username=username).first()
@@ -77,7 +122,14 @@ def get_user_profile():
 
 
 @user_bp.route("/search_users", methods=['GET'])
+@cross_origin()
 def search_users():
+    """
+    Searches for users based on a query.
+
+    Returns:
+        jsonify: A JSON response containing information about users matching the search query.
+    """
     search_query = request.args.get('query', '')
 
     similar_users = Users.objects(firstname__icontains=search_query)
