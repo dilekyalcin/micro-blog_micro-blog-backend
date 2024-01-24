419d7e73d99c62c1b6f010e59883460c318ff30b
Dilek <dilekyal1@gmail.com>
2024-01-24 18:29:17 +0300

    resolved all conflicts

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -3,4 +3,3 @@ from .commentRoute import comment_bp
 from .postRoute import post_bp
 from .likeRoute import like_bp
 from .authRoute import auth_bp
-
--- backend/routes/commentRoute.py
+++ backend/routes/commentRoute.py
@@ -16,7 +16,15 @@ comment_bp = Blueprint('comment', __name__)
 @jwt_required()
 @cross_origin()
 def add_comment():
-  
+    """
+    Adds a new comment to a post.
+
+    Validates user authentication, retrieves post information,
+    and creates a new comment associated with the current user.
+
+    Returns:
+        jsonify: A JSON response indicating the success or failure of the comment addition.
+    """
     if not current_user:
         return {"error": 'User not found.'}, 404
 
@@ -42,7 +50,18 @@ def add_comment():
 @jwt_required()
 @cross_origin()
 def delete_comment(comment_id):
+    """
+    Deletes a comment identified by its ID.
 
+    Validates user authentication and ownership of the comment,
+    then deletes the comment.
+
+    Args:
+        comment_id (str): The ID of the comment to be deleted.
+
+    Returns:
+        jsonify: A JSON response indicating the success or failure of the comment deletion.
+    """
     if not current_user:
         return {"error": 'User not found.'}, 404
 
@@ -59,7 +78,18 @@ def delete_comment(comment_id):
 @jwt_required()
 @cross_origin()
 def update_comment(comment_id):
+    """
+    Updates the content of a comment identified by its ID.
 
+    Validates user authentication and ownership of the comment,
+    then updates the comment content.
+
+    Args:
+        comment_id (str): The ID of the comment to be updated.
+
+    Returns:
+        jsonify: A JSON response indicating the success or failure of the comment update.
+    """
     if not current_user:
         return {"error": 'User not found.'}, 404
 
@@ -76,11 +106,22 @@ def update_comment(comment_id):
     return {"message": 'Comment updated.'}, 200
 
 
-@comment_bp.route("/get_all_comments", methods=['GET'])
+@comment_bp.route("/get_all_comments/<post_id>", methods=['GET'])
 @jwt_required()
 @cross_origin()
-def get_all_comments():
-    comments = Comment.objects().all()
+def get_comments_by_post(post_id):
+    """
+    Retrieves all comments on a post.
+
+    Validates user authentication and retrieves all comments associated with the specified post.
+
+    Args:
+        post_id (str): The ID of the post to retrieve comments for.
+
+    Returns:
+        jsonify: A JSON response containing information about all comments on the specified post.
+    """
+    comments = Comment.objects(post = post_id)
 
     result = []
     for comment in comments:
@@ -88,6 +129,7 @@ def get_all_comments():
             'id': str(comment.id),
             'content': comment.content,
             'author_username': comment.author.username,
+            'author_id': str(comment.author.id),
             'post': comment.post.content,
             'created_at': comment.created_at.strftime('%Y-%m-%d %H:%M:%S')
         })
